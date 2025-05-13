41b047ffe8ccfb4a627ef2a4bfa3f2a1fb16f0b1
username <useremail>
2025-05-12 21:23:42 -0700

    Add project files.

---
+++ Invoice_Line_Item_Calculator.py
@@ -0,0 +1,34 @@
+# Course CIS261 Week 6 lab 1 Invoice line item calculator
+# Emma Kailani Triad 05/12/2025
+
+#def get_price():
+    #get and validate a price from user input
+    # TODO: implement price input validation
+    # should return a valid float price
+    #pass
+    
+#def get_quantity():
+    #get and validate a quantity from user input
+    # TODO: implement quantity input validation
+    # should return a valid int quantity
+    #pass
+
+#def main():
+    #print("The Invoice Line TIem Calcuator\n")
+    
+    #TODO: implement main program loop
+    # get price and quantity from user
+    # calcualtor total
+    # display results
+    # ask if user wants to enter another line item
+    # contunie until user chooses to exit
+
+    #elif command == "exit":
+        #print("Bye!")
+        #break
+
+# if __name__ == "__main__":
+    #main()
+
+
+
