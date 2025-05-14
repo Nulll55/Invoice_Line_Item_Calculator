f89f98b39147ab89224b6e9ffc465ad2dc79fdf2
username <useremail>
2025-05-13 18:05:54 -0700

    adding functions and calcuations

--- Invoice_Line_Item_Calculator.py
+++ Invoice_Line_Item_Calculator.py
@@ -1,5 +1,5 @@
 # Course CIS261 Week 6 lab 1 Invoice line item calculator
-# Emma Kailani Triad 05/12/2025
+# Emma Kailani Tirado 05/12/2025
 
 def get_price():
     while True:
@@ -13,27 +13,32 @@ def get_price():
 def get_quantity():
     while True:
         try:
-            quantity = float(input('Enter quantity: '))
+            quantity = int(input('Enter quantity: '))
             return quantity
         except ValueError:
             print('Invalid number, please try again.' )
 
-#def main():
-    #print("The Invoice Line TIem Calcuator\n")
-    
-    #TODO: implement main program loop
-    # get price and quantity from user
-    # calcualtor total
-    # display results
-    # ask if user wants to enter another line item
-    # contunie until user chooses to exit
 
-    #elif command == "exit":
-        #print("Bye!")
-        #break
+def display_info(price, quantity, total):
+    print(f'\nPRICE:    {price: .2f}')
+    print(f'QUANTITY:   {quantity}')
+    print(f'TOTAL:      {total: .2f}')
 
-# if __name__ == "__main__":
-    #main()
+def main():
+    print("The Invoice Line TIem Calcuator\n")
+    answer = "y"
+    while answer.lower() =="y":
+        price = get_price
+        quantity = get_quantity
+        total = price * quantity
+        display_info(price, quantity, total)
+        answer = input("Enter another line item? (y/n): ")
+        print()
+    else:
+        print("Bye!")
+
+if __name__ == "__main__":
+    main()
 
 
 
